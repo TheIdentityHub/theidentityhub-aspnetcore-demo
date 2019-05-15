61c0f9cd1a4cd8449b88924793059208c6e3b647
Hannes Decorte <hannes_decorte@hotmail.com>
2019-05-15 09:10:51 +0200

    Add aspnet.core 2.2 demo app

---
+++ 2.2/AspNetCoreWebApp/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreWebApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        [Authorize]
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+    }
+}
